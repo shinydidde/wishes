a83ceabfee43fd027a19b8adeb9f4458ae05750e
User <user@admins-MacBook-Pro.local>
2021-06-07 13:37:19 +0530

    priya

--- wedding/js/main.js
+++ wedding/js/main.js
@@ -252,7 +252,7 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Sep 23, 2020 10:00:00").getTime();
+		var countDownDate = new Date("Jul 01, 2021 10:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
